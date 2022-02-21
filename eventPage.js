f88ad55b7704efc1cc42697a7dcec6d141b19015
cesarfeliix <86745043+cesarfeliix@users.noreply.github.com>
2022-02-21 10:15:06 -0300

    Create eventPage.js

---
+++ eventPage.js
@@ -0,0 +1,9 @@
+var contextMenuItem = {
+    "id": "cnpj",
+    "title": "Temple CNPJ",
+    "contexts": ["page"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+//let's see if this will commit
